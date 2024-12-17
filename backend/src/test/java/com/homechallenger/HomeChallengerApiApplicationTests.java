e2141915f68b46c20eab3cd990025c1c859a794a
Elian Condor <econdor@krugerschool.edu.ec>
2024-12-17 15:59:31 -0800

    #1 Structure Initial about backend with Java

---
+++ backend/src/test/java/com/homechallenger/HomeChallengerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homechallenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeChallengerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
