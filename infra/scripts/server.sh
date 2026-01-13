ceaaf6f93236ace986cbe9ad90a2fb510e098f4d
ejcondorf88 <eliancondor2523658@gmail.com>
2026-01-12 22:11:20 -0500

    feature: update Docker setup with environment configuration and enhance server script

--- infra/scripts/server.sh
+++ infra/scripts/server.sh
@@ -17,9 +17,9 @@ usermod -aG docker ec2-user
 
 # Ejecutar contenedor (ahora Docker puede usar el puerto 80)
 docker container run -d -p 80:80 --name fronted ejcondorf88/fronted:1.4.0
-#docker container run -d -p 8080:8080 --name backend ejcondorf88/backend:1.4.0
+docker container run -d -p 8080:8080 --name backend  --env-file .env ejcondorf88/backend:1.4.0
 docker network create fronted-backend-network
 docker network connect fronted-backend-network fronted
-#docker network connect fronted-backend-network backend
+docker network connect fronted-backend-network backend
 docker volume create portainer_data
 docker container run -d -p 9000:9000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
